0306763299a29548fa92ea7476c8fb265aa154ee
akshay sharma <akshay.3.1999@gmail.com>
2019-03-10 11:32:23 +0530

    associations in model layer

--- src/models/index.js
+++ src/models/index.js
@@ -35,5 +35,54 @@ const Users =  db.define('user',{
 })
 
 const Articles = db.define('article',{
-    slug:
+    slug:{
+        type:Sequelize.STRING,
+        primaryKey:true
+    },
+    title:{
+        allowNull:false,
+        type:Sequelize.STRING(50)
+    },
+    description:Sequelize.STRING(100),
+    body:Sequelize.STRING
 })
+
+
+const Comments = db.define('comment', {
+    body: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  })
+
+  
+
+const Tags = db.define('tag', {
+    name: {
+      type: Sequelize.STRING,
+      primaryKey: true
+    }
+  })
+  
+
+  Articles.hasMany(Comments)
+  Comments.belongsTo(Articles)
+
+  Articles.belongsTo(Users,{as : 'author'})
+  Users.hasMany(Articles)
+
+  Comments.belongsTo(Users , {as:'author'})
+
+  Articles.belongsToMany(Users , {through:'favorites'})
+  Users.belongsToMany(Articles , {through:'favorites'})
+  
+  Articles.belongsToMany(Tags , {through:'article_tags'})
+  Tags.belongsToMany(Articles , {through:'article_tags'})
+  
+  
+module.exports = {
+    db,
+    Users, Articles, Comments, Tags
+  }
+  
+  
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const {db} = require('./models')
 const app = express()
 
 app.use(express.json())
@@ -7,4 +7,8 @@ app.use(express.urlencoded({extended:true}))
 
 app.use('/api',require('./routes/api'))
 
-app.listen(1234,()=>{console.log("server started on http://localhost:1234")})
+db.sync().then(()=>{
+    app.listen(1234,()=>{console.log("server started on http://localhost:1234")})
+}).catch(err => {
+    console.error(err)
+  })
